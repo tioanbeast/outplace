66e62005c1d4cea3d4926e8f9cfc632f960b7dba
Zachary Will <Tio@ZacharysiMac.local>
2014-01-13 19:43:36 -0500

    panda.rb

--- config/initializers/panda.rb
+++ config/initializers/panda.rb
@@ -1,1 +1,1 @@
-Panda.configure((ENV['PANDASTREAM_URL'] || YAML::load_file(File.join(File.dirname(__FILE__),"..", "panda.yml"))[Rails.env]))
+Panda.configure(YAML.load_file(Rails.root.join("config/panda.yml"))[Rails.env])
