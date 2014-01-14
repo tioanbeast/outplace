8c6b171bed0be0c0e9f6d74074c1f9279d343bce
Zachary Will <Tio@ZacharysiMac.local>
2014-01-13 19:33:05 -0500

    first

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+PandaExampleRails::Application.routes.draw do
+  resources :videos
+
+  match "/panda/authorize_upload", :to => "panda#authorize_upload"
+  match "/panda/authorize_upload_postprocess", :to => "panda#authorize_upload_postprocess"
+
+  match "/upload", :to => "videos#simple"
+
+  match "/home", :to => "videos#advanced"
+
+  match "/videos/postprocess", :to => "videos#postprocess"
+
+  root :to => "videos#simple"
+
+end
