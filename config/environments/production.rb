0e70786dd89e9dfb95fefeda2a14c44050710d5e
Zachary Will <Tio@ZacharysiMac.local>
2014-01-14 15:45:30 -0500

    please work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ PandaExampleRails::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
-
+  config.assets.initialize_on_precompile=false
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
